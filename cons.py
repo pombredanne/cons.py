3415b5c2b5a2d1666a71bff140ad88b9620df4c8
Thomas Levine <occurrence@thomaslevine.com>
2013-09-24 20:23:10 -0700

    remove negatives because those do not happen

--- cons.py
+++ cons.py
@@ -58,21 +58,10 @@ class Cons:
         >>> Cons(98, Cons(2, Cons(3, Cons(8, Cons(2, None)))))[:-2]
         98, 2, 3
         '''
-        return self.slice(i, j)
-
-    def slice(self, i, j):
-        '''
-        >>> Cons(98, Cons(2, Cons(3, Cons(8, Cons(2, None))))).slice(0, -2)
-        98, 2, 3
-        '''
         if j > 0:
             return self.drop(i).take(j - i)
         elif j == 0:
             return None
-        elif j == -1:
-            return self.drop(i)
-        elif j < -1:
-            return self.init().slice(0, j + 1)
 
     def take(self, n):
         '''
